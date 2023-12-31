511149657c3aa5a182b4fb17342479f19e3d28cd
Owen Miller <owen9825@gmail.com>
2023-12-31 14:09:15 +1100

    Parsing events and replacing in destination file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Meetup Sync
-This program analyzes a saved Meetup page, exporting its saved events to JSON.
+This program extracts events from a saved Meetup page, converts the dates to Unix time, then saves the event list on
+a new page.
+
+By saving the events with Unix time, we can embed some JavaScript on a page to easily convert the times when the page
+is served.
--- time_replacement.py
+++ time_replacement.py
@@ -2,6 +2,7 @@ import argparse
 import datetime
 import io
 import re
+from typing import Optional
 
 from bs4 import BeautifulSoup
 
@@ -53,27 +54,123 @@ def get_datetime_from_element(time_element) -> datetime.datetime:
     return get_datetime_from_string(time_string=time_element.get_text())
 
 
-def replace_time_strings(source_filename: str):
-    with io.open(source_filename, "w") as source_file:
-        html_content = source_file.read()
-        # Create a Beautiful Soup object
-        soup = BeautifulSoup(html_content, 'html.parser')
+# If this is missing, either there are no events, or Meetup has changed their HTML.
+RELIABLE_DIV_ID = "e-1"  # Event 1
+HIDDEN_CLASS = "hidden"  # The destination website is meant to hide any element with this CSS class.
+DESTINATION_ID = "eventList"  # The id in the destination file
 
-        # Find the 'time' element
-        for time_element in soup.find_all('time'):
-            # Replace with Unix timestamp
-            time_element.text = int(get_datetime_from_element(time_element).timestamp() * 1_000)
-            logger(f"A time element has been replaced with {time_element.text}")
-        print(source_file.write(soup.prettify()))
+
+def get_event_ul(soup) -> Optional[BeautifulSoup]:
+    reliable_div = soup.find("div", id=RELIABLE_DIV_ID, recursive=True)  # This seems the most reliable
+    if not reliable_div:
+        logger.error("Could not find %s in this soup of %s characters", RELIABLE_DIV_ID, len(soup.text))
+        return None
+    ul = reliable_div.find_parent("ul")
+    if not ul:
+        logger.error("Element %s lacks a ul as an ancestor", RELIABLE_DIV_ID)
+        return None
+    return ul
+
+
+def get_event_list_with_unix_times(soup: BeautifulSoup) -> Optional[BeautifulSoup]:
+    event_list = get_event_ul(soup)
+    if not event_list:
+        return None
+    times = event_list.find_all("time", recursive=True)
+    for time_element in times:
+        # Replace with Unix timestamp
+        time_element.string.replace_with(str(int(get_datetime_from_element(time_element).timestamp() * 1_000)))
+    return event_list
+
+
+def parse_events_from_file(source_filename) -> Optional[BeautifulSoup]:
+    with io.open(source_filename) as source_file:
+        html_content = source_file.read()
+    source_soup = BeautifulSoup(html_content, "html.parser")
+    return get_event_list_with_unix_times(source_soup)
+
+
+def hide_events(event_list: Optional[BeautifulSoup], hide_finished_events: bool,
+                visible_population: Optional[int]) -> None:
+    # Setting visible_population to null means that all events will be visible
+    if not event_list:
+        return
+    event_lis = event_list.find_all("li", recursive=False)  # Only immediate children
+    displayed = 0
+    now_ms = datetime.datetime.utcnow().timestamp() * 1_000
+    for e, event_li in enumerate(event_lis):
+        event_time = event_li.find("time", recursive=True)
+        if event_time:
+            time_ms = int(event_time.text)
+        else:
+            child_div = event_li.find("div", recursive=True)
+            if child_div:
+                label = child_div["id"]
+            else:
+                label = f"element {e}"
+            logger.info("No time found for event %s", label)
+            time_ms = None
+        if time_ms and time_ms < now_ms and hide_finished_events:
+            if not event_li.get("class"):
+                event_li["class"] = []
+            event_li["class"].append(HIDDEN_CLASS)
+        elif isinstance(visible_population, int) and displayed >= visible_population:
+            if not event_li.get("class"):
+                event_li["class"] = []
+            event_li["class"].append(HIDDEN_CLASS)
+        else:
+            displayed += 1
+    logger.info("%s / %s events are to be displayed", displayed, len(event_lis))
+
+
+def replace_events_in_file(source_filename: str, destination_filename, hide_finished_events: bool,
+                           visible_population: int):
+    # Read some events on one page, and inject a modified version into a destination file.
+    events = parse_events_from_file(source_filename=source_filename)
+    if not events:
+        logger.warning("Events could not be parsed from %s", source_filename)
+        return
+    hide_events(events, hide_finished_events=hide_finished_events, visible_population=visible_population)
+    if not destination_filename:
+        logger.info(events.prettify("utf-8").decode())
+        return
+    with io.open(destination_filename, "r") as destination_file:
+        html_content = destination_file.read()
+    destination_soup = BeautifulSoup(html_content, "html.parser")
+    destination_events = destination_soup.find("ul", id=DESTINATION_ID, recursive=True)
+    if not destination_events:
+        logger.error("Element %s could not be found in %s", DESTINATION_ID, destination_filename)
+        return
+    events["id"] = DESTINATION_ID  # We need to be able to find it again next time.
+    destination_events.replace_with(events)
+    with io.open(destination_filename, "w") as destination_file:
+        destination_file.write(destination_soup.prettify("utf-8").decode())
+    logger.info("Events have been written to %s", destination_filename)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="This program searches for <time> elements in a local HTML file, and replaces strings with Unix "
-                    "timestamps, ready to be converted into the user's timezone, in their browser.")
+        description="This program searches for a <ul> in a local HTML file. The constituent <time> elements in the <ul>"
+                    " are replaced with Unix timestamps (so that they can be converted later when rendered), then this "
+                    "revised <ul> is saved to a new file.")
     parser.add_argument(
-        "--file",
+        "--source",
         type=str,
-        help="The HTML file to overwrite",
+        required=True,
+        help="The HTML file to read, for events",
+    )
+    parser.add_argument(
+        "--destination",
+        type=str,
+        required=False,
+        help="The HTML file where events should be written",
+    )
+    parser.add_argument(
+        "--visible-population",
+        type=int,
+        default=2,
+        help="The number of events that should be visible, before 'show more' is displayed",
     )
     args = parser.parse_args()
+    replace_events_in_file(source_filename=args.source, destination_filename=args.destination,
+                           hide_finished_events=False, visible_population=args.visible_population)
